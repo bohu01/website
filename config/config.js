53195db2ca950383cf663c1a351b67538c963e66
Bo Hu <bohu01@gmail.com>
2016-04-24 10:48:00 -0700

    first commit

--- config/config.js
+++ config/config.js
@@ -2,28 +2,28 @@ module.exports = {
   development: {
     root: require('path').normalize(__dirname + '/..'),
     app: {
-      name: 'HUAWO API',
+      name: 'website',
     },
-    db:{
-      database:"huawo_admin",
-      username:"api",
-      password:"huawo2015",
-      options:{
-        dialect: "postgres",
-        host:"gcds.cyqvivtxbihc.us-west-1.rds.amazonaws.com",
-        port:"5432",
-        pool:{
-          maxConnections: 5,
-          minConnections: 0,
-          maxIdleTime: 10000
-        }
-      }
-    }
+    // db:{
+    //   database:"huawo_admin",
+    //   username:"api",
+    //   password:"huawo2015",
+    //   options:{
+    //     dialect: "postgres",
+    //     host:"gcds.cyqvivtxbihc.us-west-1.rds.amazonaws.com",
+    //     port:"5432",
+    //     pool:{
+    //       maxConnections: 5,
+    //       minConnections: 0,
+    //       maxIdleTime: 10000
+    //     }
+    //   }
+    // }
   },
   production: {
     root: require('path').normalize(__dirname + '/../..'),
     app: {
-      name: 'HUAWO API',
+      name: 'website',
     },
     db:{}
   },
